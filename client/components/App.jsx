edd790ef268f0c7bc39fa96e2c9a7e7f003afe93
eda <kimmirindel97@gmail.com>
2018-02-09 09:54:02 +1300

    Removed unneeded components.

--- client/components/App.jsx
+++ client/components/App.jsx
@@ -3,15 +3,11 @@ import React from 'react'
 export default class App extends React.Component {
   constructor (props) {
     super(props)
-
   }
-
-
 
   render () {
     return (
-     
+      <h1>Test</h1>
     )
   }
 }
-
