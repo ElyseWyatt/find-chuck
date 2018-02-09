3edeb552ea1241b807143a2f5a94d33e57b1e4ef
eda <kimmirindel97@gmail.com>
2018-02-09 15:19:59 +1300

    polygon showing with no popups

--- client/components/Map.jsx
+++ client/components/Map.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 // import { render } from 'react-dom'
-import { Map, Marker, Popup, TileLayer } from 'react-leaflet'
+import { Map, Marker, Popup, TileLayer, Polygon } from 'react-leaflet'
+// const data = require('../../public/challengeData.json')
 
 class NewMap extends React.Component {
   constructor (props) {
@@ -12,19 +13,27 @@ class NewMap extends React.Component {
     }
   }
 
+
+
   render () {
     const position = [this.state.lat, this.state.lng]
+    const polygon = [[51.515, -0.09], [51.52, -0.1], [51.52, -0.12]]
+    const positionOne = [this.state.lat, this.state.lng];
     return (
+
       <Map center={position} zoom={this.state.zoom}>
         <TileLayer
           url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
           attribution="&copy; <a href=&quot;http://osm.org/copyright&quot;>OpenStreetMap</a> contributors"
         />
-        <Marker position={position}>
+        <Polygon color="purple" positions={polygon} />
+        
+        {/* <Marker position={positionOne}>
           <Popup>
-            <span>Wheres Chuck??</span>
+            <span>A pretty CSS3 popup. <br/> Easily customizable.</span>
           </Popup>
-        </Marker>
+        </Marker> */}
+
       </Map>
     )
   }
