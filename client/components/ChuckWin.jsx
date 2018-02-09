9e4f3e395d4c17f7dd2ef17b407d3be17b3b6007
Julia Niall <jnmareike@gmail.com>
2018-02-09 15:26:46 +1300

    Added styling and facts.

---
+++ client/components/ChuckWin.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+class ChuckWin extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+
+    }
+  }
+
+  render() {
+
+    return (
+      <div>
+        <img src="/chuck norris.jpg"></img>
+      </div>
+
+    )
+  }
+}
+
+export default ChuckWin
